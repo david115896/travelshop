b921608b0267c41960b44e55d1bb15b79cf9f323
Floury-Fabio <floury-fabio@hotmail.fr>
2019-11-25 14:52:14 +0100

    [finish] activity table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+  resources :activities
   devise_for :users
 
   devise_scope :user do
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_101957) do
+ActiveRecord::Schema.define(version: 2019_11_25_132816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
