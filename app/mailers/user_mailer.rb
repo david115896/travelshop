a941a30fe8b81b404244d476e657b4376460a556
Floury-Fabio <floury-fabio@hotmail.fr>
2019-11-26 17:10:22 +0100

    [finish] password update mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,4 +11,12 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
 
+  def password_email(user)
+    @user = user 
+
+    @url  = 'https://travel-shop.herokuapp.com/activities'
+
+    mail(to: @user.email, subject: 'Changement de mot de passe') 
+  end
+
 end
