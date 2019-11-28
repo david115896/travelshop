95d0aae17d56c2a3f54f4b4aa630065134d91c31
david115896 <dtaccori@gmail.com>
2019-11-28 16:24:32 +0100

    rename roots

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   resources :sold_items
   resources :orders, only:[:index, :create, :destroy]
-  resources :carts, only:[:index, :create, :destroy]
+  resources :carts, only:[:index, :create, :destroy], :path => "my_cart"
 
-  resources :activities do
+  resources :activities, :path => "activities" do
     collection { post :import}
   end
   devise_for :users
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     root :to => 'activities#index'
   end
-  resources :users, except:[:index, :new, :create]
+  resources :users, except:[:index, :new, :create], :path => "my_profile"
 	resources :charges, only:[:index, :new, :create]
 
 end
